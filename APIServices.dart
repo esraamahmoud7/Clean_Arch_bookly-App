9b7baa7a70fc9d6b9f02b99dae7fab4e9141910c
esraamahmoud7 <101475325+esraamahmoud7@users.noreply.github.com>
2025-06-22 22:50:47 +0300

    API Services

---
+++ APIServices.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+
+class APIServices
+{
+  final Dio dio;
+  final String baseURL = "https://www.googleapis.com/books/v1/";
+
+  APIServices({required this.dio});
+
+
+  Future<Map<String,dynamic>> get({required String endPoint})async
+  {
+    var response = await dio.get('$baseURL$endPoint');
+
+    return response.data ;
+  }
+}
